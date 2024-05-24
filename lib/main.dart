df0ea2c07fb5a7d79753bb5b027bdfac6ee60570
raymundo23t <raymundotorres255@gmail.com>
2024-05-23 20:16:59 -0600

    title

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Consultorio Médico',
+      title: 'Consultas Médicas',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
