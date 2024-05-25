3b52f136be10262695211c2ba8f8ee6b5b02984e
Zarazua-0028 <20690028@tecvalles.mx>
2024-05-24 20:37:19 -0600

    fechas

--- lib/doctor.dart
+++ lib/doctor.dart
@@ -4,13 +4,11 @@ class Doctor {
   final String name;
   final String specialty;
   final String imageUrl;
-  final Map<DateTime, List<String>> availableAppointments;
 
   Doctor({
     required this.name,
     required this.specialty,
     required this.imageUrl,
-    required this.availableAppointments,
   });
 }
 
@@ -19,19 +17,11 @@ List<Doctor> doctors = [
     name: 'Dr. Selina Zamora',
     specialty: 'Pediatra',
     imageUrl: '',
-    availableAppointments: {
-      DateTime(2024, 5, 25): ['9:00 am - 10:00 am', '2:00 pm - 3:00 pm'],
-      DateTime(2024, 5, 26): ['10:00 am - 11:00 am', '3:00 pm - 4:00 pm'],
-    },
   ),
   Doctor(
     name: 'Dr. Anthony Fauci',
     specialty: 'Cardiólogo',
     imageUrl: '',
-    availableAppointments: {
-      DateTime(2024, 5, 25): ['10:00 am - 11:00 am', '3:00 pm - 4:00 pm'],
-      DateTime(2024, 5, 26): ['11:00 am - 12:00 pm', '4:00 pm - 5:00 pm'],
-    },
   ),
 ];
 
@@ -59,16 +49,17 @@ class _DoctorProfilePageState extends State<DoctorProfilePage> {
   @override
   void initState() {
     super.initState();
+    _generateAppointmentTimes();
+  }
+
+  void _generateAppointmentTimes() {
     for (int i = 9; i <= 15; i++) {
       if (i < 12) {
-        appointmentTimes.add('$i:00 am');
-        appointmentTimes.add('$i:30 am');
+        appointmentTimes.add('$i:00 am - ${i + 1}:00 am');
       } else if (i == 12) {
-        appointmentTimes.add('$i:00 pm');
-        appointmentTimes.add('$i:30 pm');
+        appointmentTimes.add('$i:00 pm - 1:00 pm');
       } else {
-        appointmentTimes.add('${i - 12}:00 pm');
-        appointmentTimes.add('${i - 12}:30 pm');
+        appointmentTimes.add('${i - 12}:00 pm - ${i - 11}:00 pm');
       }
     }
   }
@@ -78,16 +69,6 @@ class _DoctorProfilePageState extends State<DoctorProfilePage> {
       selectedDate = date;
       selectedAppointment = null; // Reset selected appointment
     });
-  }
-
-  List<String> getAvailableAppointments(DateTime date) {
-    List<String> availableSlots = [];
-    widget.doctor.availableAppointments.forEach((key, value) {
-      if (key == date) {
-        availableSlots.addAll(value);
-      }
-    });
-    return availableSlots;
   }
 
   @override
@@ -143,41 +124,37 @@ class _DoctorProfilePageState extends State<DoctorProfilePage> {
                   Wrap(
                     spacing: 8.0,
                     runSpacing: 8.0,
-                    children: getAvailableAppointments(selectedDate!)
-                        .map((time) => ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          selectedAppointment = time;
-                        });
-                      },
-                      style: ButtonStyle(
-                        backgroundColor:
-                        MaterialStateProperty.resolveWith(
-                              (states) {
+                    children: appointmentTimes.map((time) {
+                      return ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            selectedAppointment = time;
+                          });
+                        },
+                        style: ButtonStyle(
+                          backgroundColor:
+                          MaterialStateProperty.resolveWith((states) {
                             if (selectedAppointment == time) {
                               return Colors.blueGrey;
                             } else {
                               return null;
                             }
-                          },
-                        ),
-                        padding:
-                        MaterialStateProperty.all<EdgeInsets>(
-                          EdgeInsets.symmetric(
-                              vertical: 8.0, horizontal: 4.0),
-                        ),
-                        shape: MaterialStateProperty.all<
-                            RoundedRectangleBorder>(
+                          }),
+                          padding: MaterialStateProperty.all<EdgeInsets>(
+                            EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                          ),
+                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                             RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(8.0),
-                            )),
-                      ),
-                      child: Text(
-                        time,
-                        style: TextStyle(fontSize: 12),
-                      ),
-                    ))
-                        .toList(),
+                            ),
+                          ),
+                        ),
+                        child: Text(
+                          time,
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      );
+                    }).toList(),
                   ),
                   SizedBox(height: 20),
                   Visibility(
@@ -186,17 +163,16 @@ class _DoctorProfilePageState extends State<DoctorProfilePage> {
                       onPressed: () {
                         // Implementa la lógica para confirmar la cita aquí
                         // Guarda el valor del horario seleccionado
-                        print(
-                            'Horario seleccionado: $selectedAppointment');
+                        print('Horario seleccionado: $selectedAppointment');
                       },
                       style: ButtonStyle(
                         backgroundColor:
                         MaterialStateProperty.all<Color>(Colors.blue),
-                        shape: MaterialStateProperty.all<
-                            RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8.0),
-                            )),
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
                       ),
                       child: Text(
                         'Confirmar Cita',
@@ -206,54 +182,6 @@ class _DoctorProfilePageState extends State<DoctorProfilePage> {
                   ),
                 ],
               ),
-            SizedBox(height: 20),
-            Text(
-              'Horarios Disponibles:',
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(height: 10),
-            Expanded(
-              child: GridView.count(
-                crossAxisCount: 4,
-                crossAxisSpacing: 8.0,
-                mainAxisSpacing: 8.0,
-                children: appointmentTimes
-                    .map(
-                      (time) => ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        selectedAppointment = time;
-                      });
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.resolveWith(
-                            (states) {
-                          if (selectedAppointment == time) {
-                            return Colors.green;
-                          } else {
-                            return null;
-                          }
-                        },
-                      ),
-                      padding: MaterialStateProperty.all<EdgeInsets>(
-                        EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
-                      ),
-                      shape:
-                      MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
-                      ),
-                    ),
-                    child: Text(
-                      time,
-                      style: TextStyle(fontSize: 12),
-                    ),
-                  ),
-                )
-                    .toList(),
-              ),
-            ),
           ],
         ),
       ),
@@ -282,28 +210,7 @@ class DoctoresPage extends StatelessWidget {
                 backgroundImage: NetworkImage(doctor.imageUrl),
               ),
               title: Text(doctor.name),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(height: 2.0),
-                  Text('Especialidad: ${doctor.specialty}'),
-                  SizedBox(height: 4.0),
-                  Text('Citas disponibles:'),
-                  SizedBox(height: 4.0),
-                  ...doctor.availableAppointments.entries.map((entry) {
-                    DateTime date = entry.key;
-                    List<String> slots = entry.value;
-                    return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                            '${date.day}/${date.month}/${date.year}: ${slots.join(", ")}'),
-                        SizedBox(height: 8.0),
-                      ],
-                    );
-                  }).toList(),
-                ],
-              ),
+              subtitle: Text('Especialidad: ${doctor.specialty}'),
               onTap: () {
                 Navigator.push(
                   context,
